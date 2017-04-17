aa76209cf158d7f7149da8481f235d68fe85fdfb
Knuckles9090 <joona.rahko@gmail.com>
2017-04-17 21:31:08 +0300

    Bla bla bla

--- config.js
+++ config.js
@@ -1,15 +1,4 @@
 module.exports = {
     host: process.env.NODE_HOST || 'localhost',
-    port: process.env.PORT || '3000',
-    app: {
-        htmlAttributes: { lang: 'en' },
-        title: 'React Cool Starter',
-        titleTemplate: 'React Cool Starter - %s',
-        meta: [
-            {
-                name: 'description',
-                content: 'The best react universal starter boilerplate in the world.'
-            }
-        ]
-    }
+    port: process.env.PORT || '3000'
 };
