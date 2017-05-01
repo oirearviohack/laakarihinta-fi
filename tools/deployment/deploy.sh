104db06aa6387f062e1c0eed29c132385d9c80f6
Knuckles9090 <joona.rahko@gmail.com>
2017-05-01 19:59:20 +0300

    [Feature] Eben

--- tools/deployment/deploy.sh
+++ tools/deployment/deploy.sh
@@ -21,22 +21,6 @@ deploy_cluster() {
         echo "Error updating service."
         return 1
     fi
-
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster wwwonderful-cluster --services aws-docker-cluster-service | \
-                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-            return 0
-        fi
-    done
-    echo "Service update took too long."
-    return 1
 }
 
 make_task_def(){
