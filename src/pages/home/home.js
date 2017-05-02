711003803344ea7f15c59304507b6a1769fb2bb9
Joona <joona.rahko@gmail.com>
2017-05-02 13:10:49 +0000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
-        When things happen, and love is real
+        JDog. for dave <3
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
         <img src={require('../../assets/svg/logo-small.svg')} alt="Logo-Small" role="presentation" />
     </div>
