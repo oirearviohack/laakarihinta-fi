3ee68c8dab0d1249dd9e1ea0f0cba685255b2209
Knuckles9090 <joona.rahko@gmail.com>
2017-05-02 07:53:29 +0300

    [Feature] Yo yo yo

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
-        When things happen
+        When things happen, and love is real
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
         <img src={require('../../assets/svg/logo-small.svg')} alt="Logo-Small" role="presentation" />
     </div>
