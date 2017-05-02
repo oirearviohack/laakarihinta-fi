cec056ae7fbe69112be3a83836de266ab82855a8
Joona <joona.rahko@gmail.com>
2017-05-02 13:34:27 +0000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
-        JDog. for dave <3
+        JDog. for dave 
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
         <img src={require('../../assets/svg/logo-small.svg')} alt="Logo-Small" role="presentation" />
     </div>
