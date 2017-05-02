720ae50c75ca80c238194013a40107e5a47d26b8
angela <angelabrownsey@gmail.com>
2017-05-02 18:42:34 +0000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,6 +4,7 @@ import React from 'react';
 const HomePage = () => (
     <div>
         JDog. for dave
+        angie.b was here woop woop
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
         <img src={require('../../assets/svg/logo-small.svg')} alt="Logo-Small" role="presentation" />
     </div>
