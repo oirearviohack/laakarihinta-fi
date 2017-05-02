50aefd7672daf05dd89ffb076de16bebd0107865
Knuckles9090 <joona.rahko@gmail.com>
2017-05-02 07:34:13 +0300

    [Feature] Try another push

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
-        Trying new things and loving
+        When things happen
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
         <img src={require('../../assets/svg/logo-small.svg')} alt="Logo-Small" role="presentation" />
     </div>
