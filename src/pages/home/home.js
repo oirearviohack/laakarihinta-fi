52d7ab0a32fab5ebcffe30d35e7840e1f3854390
Joona <joona.rahko@gmail.com>
2017-05-03 07:38:08 +0000

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
+        Unity Love
         JDog. for dave
         angie.b was here woop woop
         <img src={require('../../assets/svg/logo-large.svg')} alt="Logo" role="presentation" />
