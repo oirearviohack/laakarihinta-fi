4ab6bfeadc83702350f9b3b18cb4938a2fade867
Knuckles9090 <joona.rahko@gmail.com>
2017-05-05 20:45:29 +0300

    [Feature] Stuff

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
+import Help from 'material-ui-icons/Help';
+import FileUpload from 'material-ui-icons/FileUpload';
 
 
-const paperStyle = {
-    padding: 16
+const s = {
+    paperStyle: {
+        padding: 16
+    },
+    paperTitle: {
+        display: 'flex',
+        alignItems: 'center'
+    }
 };
 
 const HomePage = () => (
     <div className="row">
-        <div className="col-s-6">
-            <Paper elevation={4} style={paperStyle}>
-                <Typography type="title">
-                    Ohjeet
+        <div className="col-s-6 u-margin-sm-bottom">
+            <Paper elevation={4} style={s.paperStyle}>
+                <Typography type="headline" className="u-margin-xs-bottom" style={s.paperTitle}>
+                    <Help />
+                    <span className="u-margin-xxs-left">Ohjeet</span>
+                </Typography>
+                <Typography type="body">
+                    Lorea ipsudem ohjeet diipadapapapa dspapaspd apdpas pdaspd aspdpaspd sapdaspdp aspdaspdasp
                 </Typography>
             </Paper>
         </div>
         <div className="col-s-6">
-            <Paper elevation={4} style={paperStyle}>
-                <Typography type="title">
-                    Lataa kuva
+            <Paper elevation={4} style={s.paperStyle}>
+                <Typography type="headline" className="u-margin-xs-bottom" style={s.paperTitle}>
+                    <FileUpload />
+                    <span className="u-margin-xxs-left">Lataa kuva</span>
                 </Typography>
             </Paper>
         </div>
